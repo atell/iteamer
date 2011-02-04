92b8675e4b16cee1e45e04cfd6a000a685c7875f
atell <atell.wu@gmail.com>
2011-02-04 23:28:16 +0800

    first commit

---
+++ src/com/iteamer/Iteamer.java
@@ -0,0 +1,13 @@
+package com.iteamer;
+
+import android.app.Activity;
+import android.os.Bundle;
+
+public class Iteamer extends Activity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.loginpage);
+	}
+}

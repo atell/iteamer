9d353dbb7f1c2f26e71bb6c18f75feca76a6d4f4
atell <atell.wu@gmail.com>
2011-02-05 02:59:55 +0800

    iteamer

--- gen/com/iteamer/R.java
+++ gen/com/iteamer/R.java
@@ -33,51 +33,33 @@ public final class R {
         public static final int login_back=0x7f020010;
         public static final int login_head=0x7f020011;
         public static final int login_input=0x7f020012;
-        public static final int more_select=0x7f020013;
-        public static final int option=0x7f020014;
-        public static final int option_normal=0x7f020015;
-        public static final int option_selected=0x7f020016;
-        public static final int qq_btn_check=0x7f020017;
-        public static final int qq_edit_login=0x7f020018;
+        public static final int mates_tab=0x7f020013;
+        public static final int mates_tab_grey=0x7f020014;
+        public static final int mates_tab_white=0x7f020015;
+        public static final int more_select=0x7f020016;
+        public static final int option=0x7f020017;
+        public static final int option_normal=0x7f020018;
+        public static final int option_selected=0x7f020019;
+        public static final int qq_btn_check=0x7f02001a;
+        public static final int qq_edit_login=0x7f02001b;
     }
     public static final class id {
-        public static final int ImageButton02=0x7f070006;
-        public static final int LinearLayout01=0x7f070001;
-        public static final int RelativeLayout01=0x7f070010;
-        public static final int RelativeLayout02=0x7f070002;
-        public static final int TableLayout01=0x7f07000b;
-        public static final int TextView01=0x7f070005;
-        public static final int TextView02=0x7f070008;
-        public static final int faceImg=0x7f070003;
-        public static final int loginRoot=0x7f070000;
-        public static final int login_btn_login=0x7f07000a;
-        public static final int login_cb_openvibra=0x7f07000d;
-        public static final int login_cb_quite=0x7f07000f;
-        public static final int login_cb_receivegroupmsg=0x7f07000e;
-        public static final int login_cb_savepwd=0x7f070009;
-        public static final int login_cb_visible=0x7f07000c;
-        public static final int login_edit_account=0x7f070004;
-        public static final int login_edit_pwd=0x7f070007;
-        public static final int txt=0x7f070011;
-        public static final int user_face_drawable=0x7f070013;
-        public static final int user_faceback_drawable=0x7f070012;
+        public static final int user_face_drawable=0x7f070001;
+        public static final int user_faceback_drawable=0x7f070000;
     }
     public static final class layout {
-        public static final int loginpage=0x7f030000;
-        public static final int main=0x7f030001;
+        public static final int main=0x7f030000;
     }
     public static final class string {
-        public static final int app_name=0x7f040001;
-        public static final int hello=0x7f040000;
-        public static final int login=0x7f040006;
-        public static final int opt_acceptGroupMsg=0x7f04000a;
-        public static final int opt_hideLogin=0x7f040007;
-        public static final int opt_openVibra=0x7f040009;
-        public static final int opt_remember=0x7f040005;
-        public static final int opt_silent=0x7f040008;
-        public static final int strAccInputLabel=0x7f040003;
-        public static final int strInputAccHint=0x7f040002;
-        public static final int strPswInputLabel=0x7f040004;
+        public static final int app_name=0x7f040000;
+        public static final int location=0x7f040003;
+        public static final int location_tab=0x7f040004;
+        public static final int mates=0x7f040001;
+        public static final int mates_tab=0x7f040002;
+        public static final int meeting=0x7f040007;
+        public static final int meeting_tab=0x7f040008;
+        public static final int news=0x7f040005;
+        public static final int news_tab=0x7f040006;
     }
     public static final class style {
         public static final int MyCheckBox=0x7f050000;
---
+++ src/com/iteamer/TabWidget.java
@@ -0,0 +1,55 @@
+package com.iteamer;
+
+import android.app.TabActivity;
+import android.content.Intent;
+import android.content.res.Resources;
+import android.os.Bundle;
+import android.widget.TabHost;
+
+public class TabWidget extends TabActivity {
+	/** Called when the activity is first created. */
+	@Override
+	public void onCreate(Bundle savedInstanceState) {
+		super.onCreate(savedInstanceState);
+		setContentView(R.layout.main);
+
+		Resources res = getResources(); // Resource object to get Drawables
+		TabHost tabHost = getTabHost(); // The activity TabHost
+		TabHost.TabSpec spec; // Resusable TabSpec for each tab
+		Intent intent; // Reusable Intent for each tab
+
+		// Create an Intent to launch an Activity for the tab (to be reused)
+		intent = new Intent().setClass(this, IteamerActivity.class);
+		String mates = this.getResources().getString(R.string.mates);
+		String matesTab = this.getResources().getString(R.string.mates_tab);
+		spec = tabHost.newTabSpec(mates).setIndicator(matesTab,
+				res.getDrawable(R.drawable.mates_tab)).setContent(intent);
+		tabHost.addTab(spec);
+
+		// 团队号外
+		intent = new Intent().setClass(this, IteamerActivity2.class);
+		String news = this.getResources().getString(R.string.news);
+		String newsTab = this.getResources().getString(R.string.news_tab);
+		spec = tabHost.newTabSpec(news).setIndicator(newsTab,
+				res.getDrawable(R.drawable.mates_tab)).setContent(intent);
+		tabHost.addTab(spec);
+		// 会议提醒
+		intent = new Intent().setClass(this, IteamerActivity3.class);
+		String meeting = this.getResources().getString(R.string.meeting);
+		String meetingTab = this.getResources().getString(R.string.meeting_tab);
+		spec = tabHost.newTabSpec(meeting).setIndicator(meetingTab,
+				res.getDrawable(R.drawable.mates_tab)).setContent(intent);
+		tabHost.addTab(spec);
+		// 队友身影
+		intent = new Intent().setClass(this, IteamerActivity4.class);
+		String location = this.getResources().getString(R.string.location);
+		String locationTab = this.getResources().getString(
+				R.string.location_tab);
+		spec = tabHost.newTabSpec(location).setIndicator(locationTab,
+				res.getDrawable(R.drawable.mates_tab)).setContent(intent);
+		tabHost.addTab(spec);
+
+		// tabHost.setCurrentTab(2);
+
+	}
+}
